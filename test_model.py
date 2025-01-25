bfeee2ae839fb410fd1e823e323bcc002e13e561
rahulds02 <rahulduttsharma02@gmail.com>
2025-01-26 00:04:40 +0530

    Updated test code

--- test_model.py
+++ test_model.py
@@ -1,5 +1,3 @@
-# Assuming the following test functions need linting fixes
-
 import unittest
 from model import train_model
 
@@ -7,7 +5,10 @@ from model import train_model
 class TestModel(unittest.TestCase):
     def test_train_model(self):
         # Test training with mock data
-        model = train_model(data_path="test_data.csv", model_path="test_model.pkl")
+        model = train_model(
+            data_path="test_data.csv",
+            model_path="test_model.pkl"
+        )
         self.assertIsNotNone(model)
 
 
